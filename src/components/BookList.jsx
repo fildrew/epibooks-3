f43360c42276fe6cbcbf33fcbe7003af1763f795
philip <philip>
2024-03-18 14:20:23 +0100

    BOOKLIST

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -17,21 +17,34 @@ class BookList extends Component {
     return (
       <>
         <Form.group>
-          
+            <Form.Control
+              type="search"
+              placeholder="Search for a book"
+              value={this.state.searchQuery}
+              onChange={(e) => this.setState({ searchQuery: e.target.value })}
+            />
         </Form.group>
-        <Row className="g-2 mt-3">
-          {this.props.books
-            .filter((b) =>
-              b.title.toLowerCase().includes(this.state.searchQuery)
-            )
-            .map((b) => (
-              <Col xs={12} md={4} key={b.asin}>
-                <SingleBook book={b} />
-              </Col>
-            ))}
+
+        <Row className="justify-content-center mt-5">
+          <Col className= "col-8 text-center">
+              <Row className="justify-content-center">
+                  {this.props.books
+                    .filter((b) =>
+                      b.title.toLowerCase().includes(this.state.searchQuery)
+                    )
+                    .map((b) => (
+                      <Col xs={12} md={4} key={b.asin}>
+                        <SingleBook book={b} />
+                      </Col>
+                    ))}
+              </Row>
+          </Col>
+          <Col className="col-4 text-center sticky-top mt-5 h-50">
+            <CommentArea asin={this.state.bookId}/>
+          </Col>
         </Row>
       </>
-    )
+    );
   }
 }
 
